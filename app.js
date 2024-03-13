da7946fd7f25fd5bb84e696791bdc2279a2149f5
mori1365 <105076694+mori1365@users.noreply.github.com>
2024-03-13 22:11:02 +0330

    4

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ forEach(link => {
         link.classList.add('active');
     }
 })
+
+
+
+let images = document.querySelectorAll('.slide img');
+let slideshowContainer = document.querySelector('.slideshow-container');
+
+function moveSlide() {
+  // انتقال اولین تصویر به انتهای آرایه
+  slideshowContainer.appendChild(images[0]);
+  images = document.querySelectorAll('.slide img'); // به‌روزرسانی آرایه تصاویر
+
+  // انجام انتقال بصری
+  slideshowContainer.style.transition = 'none';
+  slideshowContainer.style.transform = 'translateX(0)';
+  setTimeout(() => {
+    slideshowContainer.style.transition = 'transform 2s ease';
+    slideshowContainer.style.transform = 'translateX(-33.33%)';
+  }, 100); // اندکی تاخیر برای اعمال تغییر بدون انتقال
+}
+
+// تنظیم تایمر برای حرکت دادن تصاویر هر 3 ثانیه
+setInterval(moveSlide, 3000);
