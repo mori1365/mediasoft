e3877b517a5853c4581a4196d90c5b0a0ecf8a93
mori1365 <105076694+mori1365@users.noreply.github.com>
2024-03-14 20:33:07 +0330

    7

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let menu = document.getElementById('menu');
+let navbar = document.querySelector(".navbar");
+menu.addEventListener('click', function() {
+    if(navbar.style.left==="100%"){
+        navbar.style.left = 0;
+    }else{
+        navbar.style.left = "100%"
+    }
+})
+
 const header = document.querySelector(".header");
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", this.scrollY > 140)
