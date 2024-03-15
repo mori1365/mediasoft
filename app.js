468633b189d53cc063c53daec3127cb85bbda723
mori1365 <105076694+mori1365@users.noreply.github.com>
2024-03-15 10:02:42 +0330

    9

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+
 let menu = document.getElementById('menu');
 let navbar = document.querySelector(".navbar");
 menu.addEventListener('click', function() {
     if(navbar.style.left==="100%"){
         navbar.style.left = 0;
     }else{
-        navbar.style.left = "100%"
+        navbar.style.left = "100%";
     }
 })
+window.onscroll = () =>{
+    navbar.style.left = "100%";
+}
 
 const header = document.querySelector(".header");
 window.addEventListener("scroll", function () {
